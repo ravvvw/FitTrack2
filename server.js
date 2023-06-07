72d20036c81f915d2a3c9847e31be6afc767f5ef
ravvvw <rway3517@uni.sydney.edu.au>
2023-06-07 16:15:44 +1000

    2nd

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ app.use(express.static(__dirname + '/dist'))
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
-})
+});
+
+app.get('*', function(req, res){
+    res.send('404 page not found');
+});
 
 app.listen(4000, function(){
-    console.log("server is running on port 3000");
+    console.log("server is running on port 4000");
 });
